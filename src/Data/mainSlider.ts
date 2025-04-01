d82cee4392fa7dd3fa35e9f0bdf6de633ec98f3b
Sagar <sagarsinghchouhan1705@gmail.com>
2025-04-01 19:35:38 +0530

    slider

--- src/Data/mainSlider.ts
+++ src/Data/mainSlider.ts
@@ -1,5 +1,5 @@
 export const homeSliderData=[
-    {image: './assets/slider3.png'},
+    {image: './assets/slider4.png'},
     {image: './assets/slider2.png'},
     {image: './assets/Explore New.png'},
     {image: './assets/slider1.png'}
